0ff9e94db45d19125e3abe960ee138f5e68101d6
Rogerotti <Rogerottii@gmail.com>
2016-03-07 00:04:45 +0100

    Takie tam

---
+++ source/VJPlayer/ViewModels/MediaViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using VJPlayer.Commands;
+using VJPlayer.Models;
+
+namespace VJPlayer.ViewModels
+{
+    public class MediaViewModel : BaseViewModel
+    {
+        private ICommand muteCommand;
+        public ICommand MuteCommand
+        {
+            get { return muteCommand; }
+            set
+            {
+                muteCommand = value;
+                OnPropertyChanged("MuteCommand");
+            }
+
+        }
+
+        private MediaElement mediaElement;
+
+        public MediaViewModel(Window window, MediaElement mediaElement) : base(window)
+        {
+            this.mediaElement = mediaElement;
+            MuteCommand = new MuteCommand(this);
+        }
+
+        public void Mute()
+        {
+            mediaElement.IsMuted = !mediaElement.IsMuted;
+        }
+    }
+}
