bb1dd2b4af8a996234127c27a2c30877444f2264
Ylniss <zupqa0@gmail.com>
2016-03-12 15:15:24 +0100

    Dodanie PlayCommand i modelu do ViewModel
    Napotkany problem z przekazaniem modelu do commanda.

---
+++ source/VJPlayer/Commands/PlayCommand.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace VJPlayer.Commands
+{
+    public class PlayCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            var mediaElement = parameter as MediaElement;
+            return mediaElement != null && mediaElement.Source != null;
+        }
+
+        public void Execute(object parameter)
+        {
+            var mediaElement = parameter as MediaElement;
+            if (mediaElement != null)
+            {
+                mediaElement.Play();
+                
+                //mediaPlayerIsPlaying = true; //trzeba ustawić tu model w viewmodel jakoś
+            }
+        }
+    }
+}
