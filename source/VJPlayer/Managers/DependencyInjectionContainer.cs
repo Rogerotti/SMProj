a42084e2dccea4b1f4833f6a2758d0ac9cbe429c
Rogerotti <Rogerottii@gmail.com>
2016-05-29 21:27:46 +0200

    abc

---
+++ source/VJPlayer/Managers/DependencyInjectionContainer.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Practices.Unity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VJPlayer.Models;
+using VJPlayer.ViewModels;
+using VJPlayer.Views;
+
+namespace VJPlayer.Managers
+{
+    public static class DependencyInjectionContainer
+    {
+        public static IUnityContainer Container;
+
+        public static void RegisterContainer()
+        {
+            Container = new UnityContainer();
+
+            Container.RegisterType<IYouTubeDownloaderViewModel, YouTubeDownloaderViewModel>();
+            Container.RegisterType<IYouTubePickerView, YouTubePicker>();
+            Container.RegisterType<IMediaModel, MediaModel>();
+            Container.RegisterType<ICoreWindowView, CoreWindow>();
+            Container.RegisterType<ICoreWindowViewModel, CoreWindowViewModel>();
+            Container.Resolve<CoreWindowViewModel>();
+        }
+    }
+}
